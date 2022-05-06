faf715bfe2a44660f9443f3137ff2de30549a4cf
IsaiasGustavo <99917604+IsaiasGustavo@users.noreply.github.com>
2022-05-06 15:37:00 -0300

    Add files via upload

---
+++ Project1/main.js
@@ -0,0 +1,27 @@
+let CL = 'Cavaleiro da Lua';
+let SON = 'Sonic';
+let VK = 'Vikings';
+let AEA = 'Ainda Estou Aqui';
+let SS = 'Sem Saída';
+let EN = 'Encanto';
+let PA = 'Projeto Adam';
+let HA = 'Homem Aranha Sem volta pra casa';
+let EPH = 'Euphoria';
+let AG = 'A anatomia de GREY';
+let HT = 'Hotel Transilvania';
+let PG = 'Pasión de Gavilantes';
+let ML = 'Metal Lords';
+let TWD = 'The Walking Dead';
+let OC = 'O comando';
+let CN = 'Caranguejo Negro';
+let EL = 'Elite';
+let TGD = 'The Good Doctor';
+
+
+
+
+document.getElementById("c0").onclick = function(){
+    document.getElementById("txt").innerHTML = CL;
+    document.getElementById("sinopse").innerHTML = 'Em Cavaleiro da Lua, durante um trabalho sujo, o mercenário Marc Spector (Oscar Isaac) sofre um acidente e acaba sendo abandonado por seus comparsas, ficando à beira da morte. Ele é, então, resgatado e levado para um templo egípcio, onde o deus da lua, Khonshu, lhe oferece uma segunda chance de viver em troca do seu corpo como hospedeiro.';
+    document.getElementById("bd").style.backgroundImage = "url('img/cf00.jpg')";
+}
